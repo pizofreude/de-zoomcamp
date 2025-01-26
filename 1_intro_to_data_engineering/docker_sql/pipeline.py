1d57f3c9e446153b7358c254d156e5035a4f9f95
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-01-27 00:33:08 +0800

    feat: second iteration for pipeline.py

--- 1_intro_to_data_engineering/docker_sql/pipeline.py
+++ 1_intro_to_data_engineering/docker_sql/pipeline.py
@@ -1,5 +1,18 @@
+###############################first iteration###############################
+# import pandas as pd
+
+# # Some fancy data processing stuff with pandas
+
+# print('job compeleted successfully')
+
+###############################second iteration###############################
+import sys
 import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
 
 # Some fancy data processing stuff with pandas
 
-print('job compeleted successfully')
+print(f'job compeleted successfully for day = {day}')
